230bfeb89eb2b14e9e08113afd4e95162af8169f
hechao <hechao1630@163.com>
2022-03-13 20:50:51 +0800

    icon 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <router-link to="/money"><Icon name="label" />记账</router-link>
-    |
-    <router-link to="/labels"> <Icon name="money" /> 标签</router-link>
-    |
-    <router-link to="/statistics"> <Icon name="statistics" /> 统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/money" class="item"
+      ><Icon name="label" />记账</router-link
+    >
+    <router-link to="/labels" class="item">
+      <Icon name="money" /> 标签</router-link
+    >
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics" /> 统计</router-link
+    >
+  </nav>
 </template>
 
 <script lang="ts">
@@ -15,4 +19,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+nav {
+  display: flex;
+  box-shadow: 0 0 3px reba(0, 0, 0, 0.25);
+  flex-direction: row;
+  > .item {
+    width: 33.33%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 2px 0;
+    .icon {
+      widows: 32px;
+      height: 32px;
+    }
+  }
+}
 </style>
