da4b053539674978e6136cf272a225c3d40ec6f9
hechao <hechao1630@163.com>
2022-03-13 15:01:37 +0800

    创建三个页面

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
+    <hr />
+    <div>
+      <router-link to="/money">记账</router-link>
+      |
+      <router-link to="/labels">标签</router-link>
+      |
+      <router-link to="/statistics">统计</router-link>
+    </div>
   </div>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
